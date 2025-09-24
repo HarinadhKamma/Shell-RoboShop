a50ed8c199de258421cb361d7f780191f76c2008
harinadh kamma <harinadkamma@gmail.com>
2025-09-24 23:14:59 +0530

    roboshop

--- ec2_roboshop.sh
+++ ec2_roboshop.sh
@@ -2,6 +2,8 @@
 
 ami_id="ami-09c813fb71547fc4f"
 sg_id="sg-03fb0717f5b080d3c"
+zone_id="Z0584855OLUDIFKCOOVC"
+record_name="haridevops.shop"
 
 for instance in $@
 do 
@@ -13,4 +15,22 @@ do
         ip=$(aws ec2 describe-instances  --instance-ids $instance_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     fi
       echo "$instance: $ip"
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $zone_id \
+    --change-batch '
+    {
+        "Comment": "Updating record set"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$record_name'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$ip'"
+            }]
+        }
+        }]
+    }
+    '
 done
