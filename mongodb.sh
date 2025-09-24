4ceb3353644783c86828414864a65fdac869291b
harinadh kamma <harinadkamma@gmail.com>
2025-09-24 23:49:11 +0530

    roboshop

---
+++ mongodb.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#Red is for ERROR
+#Green is for success
+#yellow is for skip
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+logs_folder="/var/log/shell-roboshop"
+script_name=$( echo $0 | cut -d "." -f1)
+log_file="$logs_folder/$script_name.log"
+
+mkdir -p $logs_folder
+echo "script started executing"
+
+if [ $id -u -eq o];then
+ echo "User have access to install "
+else
+   echo "user don't have root access"
+   exit 1
+fi
+
+ # functions receive inputs through args just like shell script args
+validate(){
+    if [ $1 -eq 0 ];then
+    echo -e " $2.... $G is Success $N"
+else
+   echo -e " $2.... $R is Fail $N"
+   exit 1
+fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+validate ?$ "Adding mongo repo"
+dnf install mongodb-org -y &>> $log_file
+validate ?$ "installing mongodb" 
+systemctl enable mongod 
+validate ?$ "enable mongodb"
+systemctl start mongod 
+validate ?$ "start mongodb"
+
