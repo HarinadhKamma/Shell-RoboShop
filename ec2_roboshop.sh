72cadbfbb724a6f5478aeceb265d16c2a8475230
harinadh kamma <harinadkamma@gmail.com>
2025-09-24 14:28:25 +0530

    roboshop

--- ec2_roboshop.sh
+++ ec2_roboshop.sh
@@ -5,7 +5,7 @@ sg_id="sg-03fb0717f5b080d3c"
 
 for instance in $@
 do 
-    instance_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=CLIEC2}]'  --query "Reservations[*].Instances[*].InstanceId" --output text)
+    instance_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --security-group-ids $sg_id  --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]'  --query 'Instances[0].InstanceId' --output text)
      
     if [ $instance !="frontend" ];then
         public_ip=$(aws ec2 describe-instances  --instance-ids $instance_id --query 'Reservations[*].Instances[0].PrivateIpAddress' --output text)
